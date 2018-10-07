88575d924e3600f8ace8b258f8994a6fd63bbbe0
Easlee Liu <liu@easlee.me>
2018-10-07 22:30:21 +0800

    IMPORT

---
+++ Assets/XTC/logger/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace XTC.Logger
+{
+	public interface ILogger
+	{
+		void LogTrace (string _category, string _message);
+		void LogDebug (string _category, string _message);
+		void LogInfo (string _category, string _message);
+		void LogWarning (string _category, string _message);
+		void LogError (string _category, string _message);
+		void LogException (string _category, System.Exception _exception);
+
+		void eventloop ();
+	}//interface
+}//namespace
---
+++ Assets/XTC/logger/SocketLogger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace XTC.Logger
+{
+	public class SocketLogger : ILogger
+	{
+		public void eventloop ()
+		{
+		}
+		public void LogTrace (string _category, string _message)
+		{
+			Debug.Log (string.Format ("[{0}] TRACE - {1}", _category, _message));
+		}
+		public void LogDebug (string _category, string _message)
+		{
+			Debug.Log (string.Format ("[{0}] DEBUG - {1}", _category, _message));
+		}
+		public void LogInfo (string _category, string _message)
+		{
+			Debug.Log (string.Format ("[{0}] INFO - {1}", _category, _message));
+		}
+		public void LogWarning (string _category, string _message)
+		{
+			Debug.LogWarning (string.Format ("[{0}] WARNING - {1}", _category, _message));
+		}
+		public void LogError (string _category, string _message)
+		{
+			Debug.LogError (string.Format ("[{0}] ERROR - {1}", _category, _message));
+		}
+		public void LogException (string _category, System.Exception _exception)
+		{
+			Debug.LogError (string.Format ("[{0}] EXCEPTION - !!!", _category));
+			Debug.LogException (_exception);
+		}
+	}//class
+}//namespace
